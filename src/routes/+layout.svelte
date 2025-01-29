4732ac5d77fb6ae35a7ae150c8b56c776baecdc1
devan74 <raghavadeva1903@gmail.com>
2025-01-29 15:13:08 +0530

    feat(Private Route):added private route function

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script>
-    import "../app.css";
-  </script>
-  
-  <slot />
-  
+  import "../app.css";
+  import { onMount } from "svelte";
+  import { goto } from "$app/navigation";
+
+  onMount(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      goto("/login");
+    }
+  });
+</script>
+
+<slot />
